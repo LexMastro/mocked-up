c0c8138c4464cccfcbc7f1d95615b9f503989948
Alexis Mastrodomenico <86946841+LexMastro@users.noreply.github.com>
2022-02-08 13:07:33 +1100

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL || "mongodb://localhost/mockup", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mockup", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
